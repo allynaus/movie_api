dfd5d19f38333a98979c68cb325746fa84e2f704
allynaus <ally.naus@gmail.com>
2024-04-04 13:53:47 -0700

    Updated to use mongoose

--- index.js
+++ index.js
@@ -1,155 +1,27 @@
-const mongoose = require('mongoose');
-const Models = require('./models.js');
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path'),
+    uuid = require('uuid'),
+    mongoose = require('mongoose'),
+    Models = require('./models.js');
+
+const app = express();
+const accesLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'),{
+    flags: 'a',
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extend: false }));
+app.use(morgan('combined', { stream: accesLogStream }));
+app.use(express.static('public'));
 
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/db', { useNewUrlParser: true, useUnifiedTopology: true});
-
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-const morgan = require('morgan');
-app.use(express.static('public'));
-app.use(morgan('common'));
-
-let topMovies = [
-    {
-        title: 'Sleeping With Other People',
-        director: 'Leslye Headland',
-        cast: 'Alison Brie, Jason Sudeikis',
-        genre: 'Romance, Comedy'
-    },
-    {
-        title: 'LaLaLand',
-        director: 'Damien Chazelle',
-        cast: 'Emma Stone, Ryan Gosling',
-        genre: 'Romance, Musical'
-    },
-    {
-        title: 'The Perks of Being a Wallflower',
-        director: 'Stephen Chbosky',
-        cast: 'Emma Watson, Logan Lerman',
-        genre: 'Romance, Drama'   
-    },
-    {
-        title: 'No Hard Feelings',
-        director: 'Gene Stupnitsky',
-        cast: 'Jennifer Lawrence, Andrew Barth Feldman',
-        genre: 'Comedy, Drama'
-    },
-    {
-        title: 'Love, Rosie',
-        director: 'Christian Ditter',
-        cast: 'Lily Collins, Sam Claflin',
-        genre: 'Romance, Comedy'
-    },
-    {
-        title: 'The Book of Eli',
-        director: 'Albert Hughes, Allen Hughes',
-        cast: 'Denzel Washington, Gary Oldman',
-        genre: 'Action, Sci-Fi'
-    },
-    {
-        title: 'Nobody',
-        director: 'Ilya Naishuller',
-        cast: 'Bob Odenkirk, Christopher Lloyd',
-        genre: 'Action, Thriller'
-    },
-    {
-
-        title: 'Your Place or Mine',
-        director: 'Aline Brosh McKenna',
-        cast: 'Reese Witherspoon, Ashton Kutcher',
-        genre: 'Romance, Comedy'
-    },
-    {
-        title: 'Little Women',
-        director: 'Gillian Armstrong',
-        cast: 'Winona Ryder, Christian Bale',
-        genre: 'Romance, Drama'
-    },
-    {
-        title: 'American History X',
-        director: 'Tony Kaye',
-        cast: 'Edward Norton, Edward Furlong',
-        genre: 'Indie Film, Drama'
-    },
-    {
-        title: 'How To Be Single',
-        director: "Christian Ditter",
-        cast: 'Dakota Johnson, Rebel Wilson',
-        genre: "Romance, Comedy"
-    },
-    {
-        title: 'The Devil Wears Prada',
-        director: 'Aline Brosh McKenna',
-        cast: 'Anne Hathaway, Meryl Streep',
-        genre: 'Comedy, Drama'
-    }
-];
-
-// Display index page
-app.get('/', (req, res) => {
-    res.send('Welcome to myFlix!');
-});
-
-//Get a list of movies 
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
-});
-
-//Get titles for list of movies
-app.get('/movies/:title', (req, res) => {
-    res.send({ Title: req.params.title })
-})
-
-//Get Director name, bio, and picture
-app.get('/movies/director/:directorName', (req, res) => {
-    res.json({'Director.Name': req.params.directorName })
-})
-
-//Get Genre by Name
-app.get('/movies/genres/:genreName', (req, res) => {
-    res.json({ 'Genre.Name': req.params.genreName })
-})
-
-//Add a user
-/*{
-    ID: isInteger,
-    Username: String,
-    Password: String,
-    Email: String,
-    Birthday: Date
-}*/
-app.post('/users', async (req, res) => {
-    await Users.findOne({ Username: req.body.Username})
-
-    .then((user) => {
-        if (user) {
-            return res.status(400).send(req.body.Username + 'already exists');
-        } else {
-            Users
-                .create({
-                    Username: req.body.Username,
-                    Password: req.body.Password,
-                    Email: req.body.Email,
-                    Birthday: req.body.Birthday
-                })
-                .then((user) =>{res.status(201).json(user) })
-                .catch((error) => {
-                    console.error(error);
-                    res.status(500).send('Error: ' + error);
-                })
-        }
-    })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error ' + error);
-    });
-});
+mongoose.connect('mongodb://localhost:27017/db', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get all users
 app.get('/users', async (req, res) => {
@@ -175,26 +47,107 @@ app.get('/users/:Username', async (req, res) => {
     });
 });
 
-//Update a user's info via username
-app.put('/users/:Username', async(req, res) => {
-    await Users.finOneAndUpdate({ Username: req.params.Username },
-        { $set:
-        {
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-        }
-    },
-    { new: true})
-    .then((updatedUser) => {
-        res.json(updatedUser);
+//Read Index Page
+app.get('/', function (req, res, next) {
+    res.send('Welcome to myFlix!');
+});
+
+//Read movie list
+app.get('/movies', (req, res) => {
+    Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
     })
     .catch((err) => {
         console.error(err);
         res.status(500).send('Error: ' + err);
-    })
+    });
 });
+
+//Read movies by title
+app.get('/movies/:Title', (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+        res.json(movie);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+//Read Genres by name
+app.get('/movies/genres/:genreName', (req, res) => {
+    Movies.fineOne({ 'Genre.Name': req.params.genreName })
+    .then((movie) => {
+        res.status(200).json(movie.Genre);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ', + err);
+    });
+});
+
+//Read directors by name
+app.get('/movies/directors/:directorName', (req, res) => {
+    Movies.findOne({ 'Director.Name': req.params.directorName })
+    .then((movie) => {
+        res.status(200).json(movie.Director);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ', + err);
+    });
+});
+
+//Add a user
+app.post('/users', async (req, res) => {
+    await Users.findOne({ Username: req.body.Username})
+
+    .then((user) => {
+        if (user) {
+            return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+            Users.create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                })
+                .then((user) =>{res.status(201).json(user) })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error: ' + error);
+                })
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error ' + error);
+    });
+});
+
+//Update a user's info via username
+app.put('/users/:Username', async (req, res) => {
+    await Users.finOneAndUpdate({ Username: req.params.Username },
+        { 
+            $set: {
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            },
+    },
+    { new: true}, 
+    ) //this line ensures the updated document is returned
+        .then((updatedUser) => {
+            res.json(updatedUser);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        })
+    });
 
 //Add a movie to a user's list of favorites
 app.post('/users/:Username/movies/:MovieID', async (req, res) => 
@@ -203,7 +156,8 @@ app.post('/users/:Username/movies/:MovieID', async (req, res) =>
         , {
             $push: { FavoriteMovies: req.params.MovieID }
         },
-        { new: true })
+        { new: true }
+        ) // this line ensures the updated document is returned
         .then((updatedUser) => {
             res.json(updatedUser);
         })
@@ -227,6 +181,22 @@ app.delete('/users/:Username', async (req, res) => {
         console.error(err);
         res.status(500).send('Error: ' + err);
     });
+});
+
+//Delete a user's favorite movie
+app.delete('/users/:Username/movies/:movieId', async (req, res) => {
+    await Users.finOneAndUpdate(
+        { Username: req.params.Username },
+        { $pull: { FavoriteMovies: req.params.MovieId } },
+        { new: true },
+    )
+        .then((updatedUser) => {
+            res.json(updatedUser);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //error handling middleware
